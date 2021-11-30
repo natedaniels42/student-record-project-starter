d97aabd01f7afdeee26755eb85ba374f1f9d8129
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-30 14:34:46 -0600

    Completed add form

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
+"use strict";
 // import data from './students.json';
-
-const students: HTMLElement | null = document.getElementById('students');
-const add: HTMLElement | null = document.getElementById('add');
-const addForm: HTMLElement | null = document.getElementById('add-form');
-
-type Student = {
-    firstName: string;
-    lastName: string;
-    course: string;
-    grade: number | string;
-    isPassing: boolean;
-}
-
-const data: Student[] = [
+const students = document.getElementById('students');
+const add = document.getElementById('add');
+const addForm = document.getElementById('add-form');
+const data = [
     {
         "firstName": "Patrick",
         "lastName": "Mahomes",
@@ -25,147 +16,143 @@ const data: Student[] = [
         "lastName": "Kelce",
         "course": "History",
         "grade": 88,
-        "isPassing": true 
-    }, 
+        "isPassing": true
+    },
     {
         "firstName": "Tyreek",
         "lastName": "Hill",
         "course": "Band",
         "grade": 76,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Aaron",
         "lastName": "Rodgers",
         "course": "Literature",
         "grade": 85,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Aaron",
         "lastName": "Jones",
         "course": "English",
         "grade": 46,
-        "isPassing": false 
+        "isPassing": false
     },
     {
         "firstName": "Davante",
         "lastName": "Adams",
         "course": "History",
         "grade": 93,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Kyler",
         "lastName": "Murray",
         "course": "Art",
         "grade": 77,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "James",
         "lastName": "Conner",
         "course": "Band",
         "grade": 50,
-        "isPassing": false 
+        "isPassing": false
     },
     {
         "firstName": "Deandre",
         "lastName": "Hopkins",
         "course": "History",
         "grade": 100,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Tom",
         "lastName": "Brady",
         "course": "Art",
         "grade": 70,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Leonard",
         "lastName": "Fournette",
         "course": "English",
         "grade": 86,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Mike",
         "lastName": "Evans",
         "course": "Literature",
         "grade": 23,
-        "isPassing": false 
+        "isPassing": false
     },
     {
         "firstName": "Chris",
         "lastName": "Godwin",
         "course": "English",
         "grade": 99,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Ben",
         "lastName": "Roethlisberger",
         "course": "Band",
         "grade": 76,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Najee",
         "lastName": "Harris",
         "course": "Art",
         "grade": 44,
-        "isPassing": false 
+        "isPassing": false
     },
     {
         "firstName": "Chase",
         "lastName": "Claypool",
         "course": "History",
         "grade": 62,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Kirk",
         "lastName": "Cousins",
         "course": "Literature",
         "grade": 82,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Dalvin",
         "lastName": "Cook",
         "course": "English",
         "grade": 94,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Adam",
         "lastName": "Thielen",
         "course": "Art",
         "grade": 99,
-        "isPassing": true 
+        "isPassing": true
     },
     {
         "firstName": "Justin",
         "lastName": "Jefferson",
         "course": "History",
         "grade": 70,
-        "isPassing": true 
+        "isPassing": true
     }
-]
-
-const studentArr: Student[] = localStorage.students ? JSON.parse(localStorage.students) : data;
-
+];
+const studentArr = localStorage.students ? JSON.parse(localStorage.students) : data;
 if (!localStorage.students) {
     localStorage.students = JSON.stringify(studentArr);
 }
-
-const populateStudentSection = (studentArr: Student[]): void => {
+const populateStudentSection = (studentArr) => {
     if (students) {
         students.innerHTML = '';
     }
-
     studentArr.forEach(student => {
         if (students) {
             students.innerHTML += `<tr>
@@ -175,43 +162,32 @@ const populateStudentSection = (studentArr: Student[]): void => {
                 <td>${student.grade}</td>
                 <td><img src=${student.isPassing ? './checkmark-24.png' : 'x-mark-24.png'}></td>`;
         }
-     
-    })
-}
-
+    });
+};
 if (add && addForm) {
     add.addEventListener('click', (event) => {
         event.preventDefault();
-
         add.style.visibility = 'hidden';
         addForm.style.display = 'contents';
-    })
-
+    });
     addForm.addEventListener('submit', (event) => {
         event.preventDefault();
-
-        const formData: FormData = new FormData(event.target);
-        const data: Student = Object.fromEntries(formData);
-       
-        studentArr.push(data);  
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        studentArr.push(data);
         localStorage.setItem('students', JSON.stringify(studentArr));
         populateStudentSection(studentArr);
         addForm.style.display = 'none';
         add.style.visibility = 'visible';
-    })
+    });
 }
-
-
-
-
 populateStudentSection(studentArr);
-/* Create a function to add student data to an array as an array of 
+/* Create a function to add student data to an array as an array of
 objects and render objects to the page
 
 Be sure your function parameters are given strict types
 
 */
-
 /* Define your data structure using a custom Type.
 https://www.digitalocean.com/community/tutorials/how-to-create-custom-types-in-typescript
 
@@ -237,4 +213,3 @@ Student
     Add a button that sorts the data based on Grade (ascending order)
     Add a button that sorts teh data based on Course (ascending order)
 */
-
